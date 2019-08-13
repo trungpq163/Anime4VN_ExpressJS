ec003773ebda1652499b31a14c8727be4b5c41a3
Quoc Trung <miyucoder@gmail.com>
2019-08-13 07:51:31 +0700

    Update user.js

--- app/controllers/user.js
+++ app/controllers/user.js
@@ -1,13 +1,13 @@
-module.exports = {
-    home: (req, res, next) => {
-        let id = req.param.id;
-        let user = db.get('users').find({
-            id: id
-        }).value();
+const db = require('../../db');
 
-        res.render('user/home', {
-            user: user
-        });
-        next();
-    }
-};
+module.exports.home = function (req, res, next) {
+    let id = req.param.id;
+    let user = db.get('users').find({
+        id: id
+    }).value();
+
+    res.render('user/home', {
+        user: user
+    });
+    next();
+}
