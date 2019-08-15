f57aa857c6fe10ce68cc93465e499e54a830e73e
Quoc Trung <miyucoder@gmail.com>
2019-08-15 14:59:20 +0700

    log out admin

--- app/routes/index.js
+++ app/routes/index.js
@@ -2,7 +2,8 @@ const express = require('express');
 const router = express.Router();
 const controller = require('../controllers/index');
 const authMiddleware = require('../middlewares/auth');
+const adminMiddleware = require('../middlewares/admin');
 
-router.get('/', authMiddleware.checkAuth, controller.index);
+router.get('/', authMiddleware.checkAuth, adminMiddleware.checkAdmin, controller.index);
 
 module.exports = router;
