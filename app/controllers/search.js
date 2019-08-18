00394ed5c432afc97498208b3c7c7ba9f9c90671
Quoc Trung <miyucoder@gmail.com>
2019-08-18 15:45:40 +0700

    done search

--- app/controllers/search.js
+++ app/controllers/search.js
@@ -1,21 +1,22 @@
 const db = require('../../db');
 
 module.exports.index = (req, res, next) => {
-    res.render('search/index');
+    let items = [];
+    res.render('search/index', {
+        items: items
+    });
     next();
 }
 
 module.exports.search = (req, res, next) => {
     let q = req.query.q;
-
-    let matchedItems = db
+    let items = db
         .get("itemsAnime")
         .value()
         .filter(function (item) {
             return item.title.toLowerCase().indexOf(q.toLowerCase()) !== -1;
         });
-    res.render('index', {
-        matchedItems: matchedItems,
+    res.render('search/search', {
         items: items
     });
     next();
