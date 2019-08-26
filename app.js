5878c20b7d0cdd6e8a29d15c637c7f376647b734
miyucoder <miyucoder@gmail.com>
2019-08-26 07:18:10 +0700

    fix some file

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 // require modules
 require(`dotenv`).config();
 const express = require(`express`);
-const methodOveride = require(`method-override`)
+const methodOveride = require(`method-override`);
 const bodyParser = require(`body-parser`);
 const cookieParser = require(`cookie-parser`);
 const path = require(`path`);
@@ -23,7 +23,6 @@ const allAnimeRoute = require(`./app/routes/allAnime`);
 app.set(`views`, `./views`);
 app.set(`view engine`, `pug`);
 
-
 // using body-parser
 app.use(bodyParser.json());
 app.use(
@@ -43,7 +42,6 @@ app.use(methodOveride(`_method`));
 app.use(express.static(path.join(__dirname, `public`)));
 
 // using routes file
-
 app.use(`/`, indexRoute);
 app.use(`/auth`, authRoute);
 app.use(`/user`, userRoute);
@@ -52,10 +50,53 @@ app.use(`/search`, searchRoute);
 app.use(`/itemsUpdate`, itemUpdateRoute);
 app.use(`/items`, itemRoute);
 app.use(`/allAnime`, allAnimeRoute);
-
-app.use(function (req, res) {
-    res.status(404).end('eror');
-    res.render('404');
+app.use(function (req, res, next) {
+    res.status(404).end();
 });
+
 // listening on port
 app.listen(port, () => console.log(`App listening on port, ` + port));
+
+// catch 404 and forward to error handler
+// app.use(function (req, res, next) {
+//     let err = new Error('Not Found');
+//     err.status = 404;
+//     next(err);
+// });
+
+// // when status is 404, error handler
+// app.use(function (err, req, res, next) {
+//     // set locals, only providing error in development
+//     res.locals.message = err.message;
+//     res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+//     // render the error page
+//     res.status(err.status || 500);
+//     if (404 === err.status) {
+//         res.format({
+//             'text/plain': () => {
+//                 res.send({
+//                     message: 'not found Data'
+//                 });
+//             },
+//             'text/html': () => {
+//                 res.render('404.pug');
+//             },
+//             'application/json': () => {
+//                 res.send({
+//                     message: 'not found Data'
+//                 });
+//             },
+//             'default': () => {
+//                 res.status(406).send('Not Acceptable');
+//             }
+//         })
+//     }
+
+//     // when status is 500, error handler
+//     if (500 === err.status) {
+//         return res.send({
+//             message: 'error occur'
+//         });
+//     }
+// });
